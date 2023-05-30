3dd71237669150a70ea27c4dcdefa8f136fbc2f1
Madis Liias <madis@qminder.com>
2023-05-30 21:42:28 +0300

    Add browsers cask
    Can be installed via:
    `brew install --cask browsers-software/tap/browsers`

---
+++ Casks/browsers.rb
@@ -0,0 +1,23 @@
+# typed: true
+# frozen_string_literal: true
+
+cask "browsers" do
+  version "0.3.5"
+  sha256 "e14872bf856d14e18f7797f642b8dd408bcc9a24eea714953100c93c58f8cc44"
+
+  url "https://github.com/Browsers-software/browsers/releases/download/#{version}/browsers_mac.tar.gz",
+      verified: "github.com/Browsers-software/browsers/"
+  name "Browsers"
+  desc "Open the right browser at the right time"
+  homepage "https://browsers.software/"
+
+  depends_on macos: ">= :el_capitan"
+
+  app "Browsers.app"
+
+  zap trash: [
+    "~/Library/Application Support/software.Browsers",
+    "~/Library/Caches/software.Browsers",
+    "~/Library/Logs/software.Browsers",
+  ]
+end
